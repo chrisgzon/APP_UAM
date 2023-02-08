2f5356259c08f757bd36b2c4286028ed8fe1a9e0
Cristian18-code <cristiangarzon180@gmail.com>
2023-02-08 09:17:51 -0500

    [Commit]:"Add 404 handled exception"

--- APP_UAM/Program.cs
+++ APP_UAM/Program.cs
@@ -28,6 +28,16 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.Use(async (context, next) =>
+    {
+        await next();
+        if (context.Response.StatusCode == 404)
+        {
+            context.Request.Path = "/Home";
+            await next();
+        }
+    });
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
